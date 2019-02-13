6367521ebbd0ffcfe6ed3289c4f791ca13f09cb0
Tebin <tebin.raouf@gmail.com>
2019-02-12 19:13:55 -0500

    update travis

--- app/main.js
+++ app/main.js
@@ -19,7 +19,7 @@ function createWindow() {
 
 
   // and load the index.html of the app.
-  mainWindow.loadURL(__dirname + '/index.html')
+  mainWindow.loadURL('file://' + __dirname + '/index.html')
 
   // Open the DevTools.
   mainWindow.webContents.openDevTools();
