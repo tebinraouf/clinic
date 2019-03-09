15ee046d81b278667f7da62cfe112b294a8c2695
Tebin <tebin.raouf@gmail.com>
2019-03-09 17:33:47 -0500

    properly place chart

--- process/js/MainContent.jsx
+++ process/js/MainContent.jsx
@@ -1,34 +1,69 @@
 /* eslint-disable */
 const React = require("react");
+
 var PieChart = require("recharts").PieChart;
 var Pie = require("recharts").Pie;
 var Sector = require("recharts").Sector;
 var Cell = require("recharts").Cell;
 var Legend = require("recharts").Legend;
 var Tooltip = require("recharts").Tooltip;
+var ResponsiveContainer = require("recharts").ResponsiveContainer;
 
-const data01 = [{name: 'Group A', value: 400}, {name: 'Group B', value: 300},
-                  {name: 'Group C', value: 300}, {name: 'Group D', value: 200},
-                  {name: 'Group E', value: 278}, {name: 'Group F', value: 189}]
 const COLORS = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042"];
 
 class MainContent extends React.Component {
-  componentDidMount() {}
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: new Array()
+    };
+  }
+
+  componentDidMount() {
+    const data = [{ name: "Male", value: 400 }, { name: "Female", value: 300 }];
+
+    this.setState({
+      data: data
+    });
+  }
 
   render() {
+    const data = this.state.data;
+
     return (
       <main className="main-content col-lg-10 col-md-9 col-sm-12 p-0 offset-lg-2 offset-md-3">
-        <PieChart width={800} height={400}>
-        <Pie isAnimationActive={true} dataKey="value" data={data01} cx={200} cy={200} outerRadius={80} fill="#8884d8" label>
-          {data01.map((entry, index) => (
-              <Cell
-                key={`cell-${index}`}
-                fill={COLORS[index % COLORS.length]}
-              />
-            ))}
-        </Pie>
-        <Tooltip/>
-       </PieChart>
+        <div className="row p-4">
+          <div className="col-sm-4">
+            <div className="card mx-auto">
+              <div className="card-body">
+                <h5 className="card-title">Graph 1</h5>
+                <ResponsiveContainer className="container" height={400}>
+                  <PieChart>
+                    <Pie
+                      isAnimationActive={true}
+                      dataKey="value"
+                      data={data}
+                      cy={200}
+                      outerRadius={120}
+                      fill="#8884d8"
+                      label
+                    >
+                      {data.map((entry, index) => (
+                        <Cell
+                          key={`cell-${index}`}
+                          fill={COLORS[index % COLORS.length]}
+                        />
+                      ))}
+                    </Pie>
+                    <Tooltip />
+                    <Legend />
+                  </PieChart>
+                </ResponsiveContainer>
+              </div>
+            </div>
+          </div>
+        </div>
       </main>
     );
   }
